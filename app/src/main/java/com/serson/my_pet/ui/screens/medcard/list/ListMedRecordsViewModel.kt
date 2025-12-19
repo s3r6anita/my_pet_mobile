4fec89c7441abcdd4a1e7b18d8f4b39266529e3f
ponomo2 <ponom.sofia03@gmail.com>
2025-12-19 20:07:59 +0300

    [26-task] added medrecords list screen and view model

---
+++ app/src/main/java/com/serson/my_pet/ui/screens/medcard/list/ListMedRecordsViewModel.kt
@@ -0,0 +1,40 @@
+package com.serson.my_pet.ui.screens.medcard.list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.serson.my_pet.data.db.Repository
+import com.serson.my_pet.data.db.entities.MedRecord
+import com.serson.my_pet.data.db.entities.Pet
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+import javax.inject.Inject
+
+@HiltViewModel
+class ListMedRecordsViewModel @Inject constructor(
+    private val repository: Repository
+) : ViewModel() {
+    private val _medRecordsUiState = MutableStateFlow(emptyList<MedRecord>())
+    val medRecordsUiState = _medRecordsUiState.asStateFlow()
+    private val _msg = MutableStateFlow("")
+    val msg = _msg.asStateFlow()
+
+    var pet = Pet(
+        "", "", "", "Самец",
+        LocalDate.now(),
+        "", "", "", 0
+    )
+
+    fun getPetsMedRecords(petId: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            pet = repository.getPetForCU(petId)
+            repository.getMedRecordsForPet(petId).collect { medRecords ->
+                _medRecordsUiState.value = medRecords
+            }
+        }
+    }
+
+}
